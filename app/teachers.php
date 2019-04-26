68dcef54e1a0e03fba8fc3e3610dda86e95a73df
Abdelrhman98 <abdelrhman.soleman98@gmail.com>
2019-04-26 02:00:49 +0200

    lol

--- app/teachers.php
+++ app/teachers.php
@@ -13,7 +13,12 @@ class teachers extends Model
     public static function GetTeacherDetails(){
         $teacher = teachers::where('teacherId', Auth::user()->id )->get();
         //dd($teacher[0]->teacherDetails);
-        return  $teacher[0]->teacherDetails ;
+        if(sizeof($teacher)>0){
+            return  $teacher[0]->teacherDetails ;
+        }else{
+            return "";
+        }
+        
         
     }
 
